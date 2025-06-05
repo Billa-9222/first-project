8a1d979cafc140030e1ee9d4b12036f48374ea94
Jamila <jam.2999billa@gmail.com>
2025-06-05 11:08:06 +0430

    Fix: Capitalized Header component import

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Header from "./components/header";
+import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import AboutMe from "./components/AboutMe";
 import Skills from "./components/Skills";
